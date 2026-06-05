a3be80be5d6743d93bf3450755da418e9e07bfca
kimhyunsoo <gustn9515@gmail.com>
2026-06-05 15:11:23 +0900

    fix: 통계 취소금액, 취소건수 추가하여 쿼리 변경

--- back-springboot/src/main/java/org/best/backspringboot/dto/statistics/DailyPaymentDto.java
+++ back-springboot/src/main/java/org/best/backspringboot/dto/statistics/DailyPaymentDto.java
@@ -6,7 +6,11 @@ import lombok.Setter;
 @Getter
 @Setter
 public class DailyPaymentDto {
-    private String date;    // yyyyMMdd
-    private Long amount;    // 총 결제금액
-    private Long count;     // 결제 건수
+    private String date;          // yyyyMMdd
+    private Long amount;          // 순 결제금액 (성공 - 취소)
+    private Long count;           // 순 결제건수 (성공 - 취소)
+    private Long successAmount;   // 성공 결제금액
+    private Long successCount;    // 성공 결제건수
+    private Long cancelAmount;    // 취소 금액
+    private Long cancelCount;     // 취소 건수
 }
